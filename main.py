bfb09c777e6ce4be7a940737194ebe25f79fc88c
Fakhri Joe Isgandarov <64754867+rush-C@users.noreply.github.com>
2025-02-12 06:50:40 -0500

    Added the character occurence functionality

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def char_counter(some_text):
+    dict = {}
+    all_chars = []
+    some_text = some_text.lower()
+    for char in some_text:
+        if char not in all_chars:
+            all_chars.append(char)
+
+    for char in all_chars:
+        dict[char] = some_text.count(char)        
+
+        
+    return(dict)
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -11,6 +26,8 @@ def main():
     book_text = get_book_text(book_path)
     word_count = word_counter(book_text)
     print(f"The book contains {word_count} words.")
+    dictionary = char_counter(book_text)
+    print(dictionary)
 
 
 
